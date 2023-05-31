2160e23883298f5102d81a86e264f98014406c76
Diallo Oumar <do25595@gmail.com>
2023-05-31 07:17:33 +0000

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormationComponent } from './components/formation/formation.component';
+import { EvenementComponent } from './components/evenement/evenement.component';
+import { SalleComponent } from './components/salle/salle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormationComponent,
+    EvenementComponent,
+    SalleComponent
   ],
   imports: [
     BrowserModule,
